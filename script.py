40194edb4f2dd7de2b90b155c9e82cf9f53a53bd
robert-bafoe <66376865+robert-bafoe@users.noreply.github.com>
2020-07-02 17:07:43 +0200

    provedl jsem opravy až k foor loopům

--- script.py
+++ script.py
@@ -30,67 +30,65 @@ garpike and stingray are also present.'''
 
 USERS = {"bob":"123","ann":"pass123","mike":"password123","liz":"pass123"}
 
-l_titledWords = 0
-l_upperWords = 0
-l_lowerWords = 0
-l_numericWords = 0
-l_sumOfNumbers = 0
+titledWords = 0
+upperWords = 0
+lowerWords = 0
+numericWords = 0
+sumOfNumbers = 0
+textCount = len(TEXTS)
 
 print("-" * 60)
 print("Welcome to TextApp. Please log in.")
 print("-" * 60)
-l_username = input("Username: ")
-l_password = input("Password: ")
-if l_username in USERS.keys():
-    if not(l_password == USERS[l_username]):
-        print("Username or password is not correct!")
-        exit()
-else:
+username = input("Username: ")
+password = input("Password: ")
+if (username not in USERS.keys()) or (password != USERS[username]):
     print("Username or password is not correct!")
     exit()
 
 print("-" * 60)
-print("We have 3 texts to be analyzed.")
-l_textNumber = int(input("Enter a number btw. 1 and 3 to select: "))-1
+print(f"We have {textCount} texts to be analyzed.")
+textNumber = int(input(f"Enter a number btw. 1 and {textCount} to select: "))-1
 print("-" * 60)
 
-l_wordsCount = len(TEXTS[l_textNumber].split())
-for word in TEXTS[l_textNumber].split():
+splittedTexts = TEXTS[textNumber].split()
+wordsCount = len(splittedTexts)
+
+for word in splittedTexts:
     if word.istitle():
-        l_titledWords += 1
+        titledWords += 1
 
-for word in TEXTS[l_textNumber].split():
+for word in splittedTexts:
     if word.isupper():
-        l_upperWords += 1
+        upperWords += 1
 
-for word in TEXTS[l_textNumber].split():
+for word in splittedTexts:
     if word.islower():
-        l_lowerWords += 1
+        lowerWords += 1
 
-for word in TEXTS[l_textNumber].split():
+for word in splittedTexts:
     if word.isnumeric():
-        l_numericWords += 1
-        l_sumOfNumbers += int(word)
+        numericWords += 1
+        sumOfNumbers += int(word)
 
-
-print(f"There are {l_wordsCount} words in the selected text.")
-print(f"There are {l_titledWords} titlecase words.")
-print(f"There are {l_upperWords} uppercase words.")
-print(f"There are {l_lowerWords} lowercase words.")
-print(f"There are {l_numericWords} numeric strings.")
+print(f"There are {wordsCount} words in the selected text.")
+print(f"There are {titledWords} titlecase words.")
+print(f"There are {upperWords} uppercase words.")
+print(f"There are {lowerWords} lowercase words.")
+print(f"There are {numericWords} numeric strings.")
 print("-" * 60)
 
-l_words = []
-l_words = TEXTS[l_textNumber].replace(",","").replace(".","").split()
-l_counts = {}
-for word in l_words:
-  if (l_counts.get(len(word))==None):
-    l_counts.setdefault(len(word),1)
+words = []
+words = TEXTS[textNumber].replace(",","").replace(".","").split()
+counts = {}
+for word in words:
+  if (counts.get(len(word))==None):
+    counts.setdefault(len(word),1)
   else:
-    l_counts[len(word)] += 1
-for key in sorted(l_counts):
-  print(f"{key} {(l_counts[key])*'*'} {l_counts[key]}")
+    counts[len(word)] += 1
+for key in sorted(counts):
+  print(f"{key} {(counts[key])*'*'} {counts[key]}")
 
 print("-" * 60)
-print(f"If we summed all the numbers in this text we would get: {float(l_sumOfNumbers)}")
+print(f"If we summed all the numbers in this text we would get: {float(sumOfNumbers)}")
 print("-" * 60)
