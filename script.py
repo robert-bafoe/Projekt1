f43d77e1816c2fc91861e51e5c9f3f2afe5c746d
robert-bafoe <66376865+robert-bafoe@users.noreply.github.com>
2020-07-02 17:37:11 +0200

    oprava zbytku drobná vylepšení kódu (try except a podmínka na zadané číslo < 1)

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ above sea level. The butte is located just
 north of US 30N and the Union Pacific Railroad, 
 which traverse the valley. ''',
 
-'''At the base of Fossil Butte are the bright 
+         '''At the base of Fossil Butte are the bright 
 red, purple, yellow and gray beds of the Wasatch 
 Formation. Eroded portions of these horizontal 
 beds slope gradually upward from the valley floor 
@@ -17,7 +17,7 @@ to the top of the butte are the much steeper
 buff-to-white beds of the Green River Formation, 
 which are about 300 feet thick.''',
 
-'''The monument contains 8198 acres and protects 
+         '''The monument contains 8198 acres and protects 
 a portion of the largest deposit of freshwater fish 
 fossils in the world. The richest fossil fish deposits 
 are found in multiple limestone layers, which lie some 
@@ -26,9 +26,9 @@ represent several varieties of perch, as well as
 other freshwater genera and herring similar to those 
 in modern oceans. Other fish such as paddlefish, 
 garpike and stingray are also present.'''
-]
+         ]
 
-USERS = {"bob":"123","ann":"pass123","mike":"password123","liz":"pass123"}
+USERS = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
 
 titledWords = 0
 upperWords = 0
@@ -37,21 +37,35 @@ numericWords = 0
 sumOfNumbers = 0
 textCount = len(TEXTS)
 
-print("-" * 60)
+print("-" * 65)
 print("Welcome to TextApp. Please log in.")
-print("-" * 60)
+print("-" * 65)
 username = input("Username: ")
 password = input("Password: ")
 if (username not in USERS.keys()) or (password != USERS[username]):
     print("Username or password is not correct!")
     exit()
 
-print("-" * 60)
+print("-" * 65)
 print(f"We have {textCount} texts to be analyzed.")
-textNumber = int(input(f"Enter a number btw. 1 and {textCount} to select: "))-1
-print("-" * 60)
+try:
+    textNumber = int(input(f"Enter a number between 1 and {textCount} to select: ")) - 1
+except:
+    print(f"You did not enter a number.")
+    exit()
 
-splittedTexts = TEXTS[textNumber].split()
+if textNumber < 1:
+    print(f"OK, this should work, but I asked you for a number between 1 and {textCount}.")
+    exit()
+
+print("-" * 65)
+
+try:
+    splittedTexts = TEXTS[textNumber].split()
+except:
+    print(f"The number is not between 1 and {textCount}.")
+    exit()
+
 wordsCount = len(splittedTexts)
 
 for word in splittedTexts:
@@ -65,25 +79,25 @@ for word in splittedTexts:
         numericWords += 1
         sumOfNumbers += int(word)
 
+print(f"Here is the text of your choice:")
+print(TEXTS[textNumber])
+print("-" * 65)
 print(f"There are {wordsCount} words in the selected text.")
 print(f"There are {titledWords} titlecase words.")
 print(f"There are {upperWords} uppercase words.")
 print(f"There are {lowerWords} lowercase words.")
 print(f"There are {numericWords} numeric strings.")
-print("-" * 60)
+print("-" * 65)
 
 words = []
-words = TEXTS[textNumber].replace(",","").replace(".","").split()
+words = TEXTS[textNumber].replace(",", "").replace(".", "").split()
 counts = {}
 for word in words:
-  if (counts.get(len(word))==None):
-    counts.setdefault(len(word),1)
-  else:
-    counts[len(word)] += 1
+    counts[len(word)] = counts.setdefault(len(word), 0) + 1
 
 for key in sorted(counts):
-  print(f"{key} {(counts[key])*'*'} {counts[key]}")
+    print(f"{key} {(counts[key]) * '*'} {counts[key]}")
 
-print("-" * 60)
+print("-" * 65)
 print(f"If we summed all the numbers in this text we would get: {float(sumOfNumbers)}")
-print("-" * 60)
+print("-" * 65)
